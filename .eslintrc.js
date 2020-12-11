71f0642cade90de3dd7054e6679636f6bd0fa08b
Marcus Spiegel <spiegel@uscreen.de>
2020-12-11 09:27:11 +0100

    replace local eslint config with @uscreen.de/eslint-config-prettystandard-node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true
-    },
-    extends: ['standard', 'plugin:prettier/recommended'],
-    rules: {
-        'generator-star-spacing': 'off',
-        'space-before-function-paren': 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-    }
+  extends: ['@uscreen.de/eslint-config-prettystandard-node']
 }
