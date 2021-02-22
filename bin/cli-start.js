90e4b870c954a1216d2c163be84329b901615745
Martin Herting <herting@uscreen.de>
2021-02-22 16:04:56 +0100

    Making `service start` check used ports before starting services.

--- bin/cli-start.js
+++ bin/cli-start.js
@@ -3,12 +3,7 @@
 const cli = require('commander')
 
 const { version } = require('../src/constants')
-
-const { compose, error } = require('../src/utils')
-/**
- * @todo enable checkUsedPorts, see below
- */
-// const { compose, checkUsedPorts, error } = require('../src/utils')
+const { compose, checkUsedPorts, error } = require('../src/utils')
 
 cli
   .version(version)
@@ -16,18 +11,10 @@ cli
   .action(async (service) => {
     try {
       if (service) {
-        /**
-         * @todo wants to have been checked before starting
-         * currently breaks test -> use `service check && service start` instead
-         */
-        // await checkUsedPorts(service)
+        await checkUsedPorts(service)
         await compose('up', '-d', service)
       } else {
-        /**
-         * @todo wants to have been checked before starting
-         * currently breaks test -> use `service check && service start` instead
-         */
-        // await checkUsedPorts()
+        await checkUsedPorts()
         await compose('up', '-d')
       }
     } catch (e) {
